1dda4a2643edf3494be6be7c59462ecb690cbb76
^_^肥仔John <fsjohnhuang@hotmail.com>
2023-10-07 16:53:47 +0800

    (test: cache) Change test configuration

--- packages/cache/jest.config.js
+++ packages/cache/jest.config.js
@@ -2,9 +2,10 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["<rootDir>/test/**/*.spec.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
+  // collect coverage information or not, defaults to false.
+  collectCoverage: true,
+  // coverageDirectory: "test/coverage", // where to output the coverage information files, defaults to console only.
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.json" }],
   },
 };
--- packages/cache/test/cache.spec.ts
+++ packages/cache/test/cache.spec.ts
@@ -1,5 +1,20 @@
+import { Cache } from "../src/core/Cache";
+
 describe("Cache", () => {
-  it("should be ", () => {
-    expect(1).toEqual(1);
+  let cache: Cache;
+  beforeEach(() => {
+    cache = new Cache();
+  });
+
+  it("should get undefined", () => {
+    const value = cache.get("undefined");
+    expect(value).toBeUndefined();
+  });
+
+  it("should throw an error", () => {
+    cache.dispose();
+    expect(() => {
+      cache.get("undefined");
+    }).toThrow(Error);
   });
 });
