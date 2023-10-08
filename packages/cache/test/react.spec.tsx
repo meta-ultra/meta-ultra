0655dadb13150314d408932525907e3db294ff3e
^_^肥仔John <fsjohnhuang@hotmail.com>
2023-10-08 15:41:55 +0800

    (test: cache) Add tests for react submodule

---
+++ packages/cache/test/react.spec.tsx
@@ -0,0 +1,83 @@
+import { FC } from "react";
+import { renderHook, render, screen, waitFor } from "@testing-library/react";
+import { Cache } from "../src/core/Cache";
+import { CacheItem } from "../src/core/types";
+import { useCache, CacheProvider } from "../src/react/CacheContext";
+import { withCache } from "../src/react/withCache";
+
+beforeAll(() => {
+  jest.spyOn(console, "error").mockImplementation(() => jest.fn());
+});
+afterAll(() => {
+  jest.restoreAllMocks();
+});
+
+describe("useCache", () => {
+  it("should throw an error when call useCache outside CacheProvider", () => {
+    expect(() => {
+      renderHook(() => useCache(), {});
+    }).toThrow(/^useCache must be inside a CacheProvider with a value$/);
+  });
+
+  it("should get a Cache instance", () => {
+    const { result } = renderHook(() => useCache(), {
+      wrapper({ children }) {
+        return <CacheProvider cache={new Cache()}>{children}</CacheProvider>;
+      },
+    });
+
+    expect(result.current).toBeInstanceOf(Cache);
+  });
+});
+
+describe("withCache", () => {
+  let WithCache: FC;
+  beforeAll(() => {
+    WithCache = withCache(
+      () => <div data-testid="success"></div>,
+      <div data-testid="fallback"></div>
+    );
+  });
+
+  it("should throw an error when render component withCache outside CacheProvider", () => {
+    expect(() => {
+      render(<WithCache />);
+    }).toThrow(/^useCache must be inside a CacheProvider with a value$/);
+  });
+
+  it("should render the internal component when cache is ready", async () => {
+    render(<WithCache />, {
+      wrapper({ children }) {
+        return <CacheProvider cache={new Cache()}>{children}</CacheProvider>;
+      },
+    });
+
+    expect(await screen.findAllByTestId("success")).toBeTruthy();
+  });
+
+  it("should render the fallback component when cache is not ready", async () => {
+    render(<WithCache />, {
+      wrapper({ children }) {
+        const storage = {
+          serialize(id: string, cache: Map<string, CacheItem>): void {
+            //
+          },
+          deserialize(id: string): Promise<Map<string, CacheItem>> {
+            return new Promise((resolve) => {
+              setTimeout(() => {
+                resolve(new Map());
+              }, 1000);
+            });
+          },
+        };
+        return (
+          <CacheProvider cache={new Cache({ storage })}>
+            {children}
+          </CacheProvider>
+        );
+      },
+    });
+
+    expect(await screen.findAllByTestId("fallback")).toBeTruthy();
+  });
+});
