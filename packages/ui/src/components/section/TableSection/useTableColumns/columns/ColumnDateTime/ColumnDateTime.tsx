468a053aadac6c370b4534ded40cdb2066ed00c9
^_^肥仔John <fsjohnhuang@hotmail.com>
2023-10-25 14:38:24 +0800

    fix(ui): return the raw value if it's undefined
    Change the logic shown below once the value of column is undefined:
    
    - return the raw value if the render function doesn't exist.
    - call render function with the result of `type: "datetime", format: "..."`
      modifier processed.
    
    closed #1

--- packages/ui/src/components/section/TableSection/useTableColumns/columns/ColumnDateTime/ColumnDateTime.tsx
+++ packages/ui/src/components/section/TableSection/useTableColumns/columns/ColumnDateTime/ColumnDateTime.tsx
@@ -5,12 +5,14 @@ interface ColumnDateTimeOption<RecordType> extends TableColumnType<RecordType> {
   format: string;
 }
 
-const ColumnDateTime = <RecordType,>(
-  option: ColumnDateTimeOption<RecordType>
-) => ({
+const ColumnDateTime = <RecordType,>(option: ColumnDateTimeOption<RecordType>) => ({
   ...option,
-  render(dateTime: unknown) {
-    return dayjs(dateTime as string | Date).format(option.format);
+  render(dateTime: unknown, record: RecordType, index: number) {
+    const result =
+      typeof dateTime === "string" || dateTime instanceof Date
+        ? dayjs(dateTime as string | Date).format(option.format)
+        : dateTime;
+    return option.render ? option.render(result, record, index) : result;
   },
 });
 
